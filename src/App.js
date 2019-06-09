4923051215ce4815ead3c134437ff6f82e89fdb1
Andrew Larson <andyson456@gmail.com>
2019-06-09 14:47:21 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './index.css';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css'
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        const data = [{
+            employee: 'Dustin ',
+            date: '6/9/2019',
+            type: '',
+            activity: '',
+          },{
+         
+          }]
+         
+          const columns = [{
+            Header: 'Date',
+            accessor: 'date'
+          }, {
+            Header: 'Employee',
+            accessor: 'employee' // String-based value accessors!
+          }, {
+            Header: 'Type',
+            accessor: 'type',
+          }, {
+            id: 'friendName', // Required because our accessor is not a string
+            Header: 'Project',
+          }, {
+            Header: 'Activity', // Custom header components!
+            accessor: 'activity'
+          }, {
+            Header: 'In'
+          }, {
+            Header: 'Out'
+          }, {
+            Header: 'Hours' 
+          }, {
+            Header: 'Notes'
+          }, {
+            Header: 'Approved'
+          }, {
+            Header: 'QuickBooks'
+          }]
+         
+          return (
+          <ReactTable
+            data={data}
+            columns={columns}
+          />)
+        
+    }
+}
+
+export default App;
