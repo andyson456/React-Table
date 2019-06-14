3d86b67f0455ac70c60f2c6662aaa521f242b907
Andrew Larson <andyson456@gmail.com>
2019-06-14 15:23:21 -0700

    Need to finish UI

--- src/Components/editTime.js
+++ src/Components/editTime.js
@@ -1,1 +1,18 @@
 import React, { Component } from 'react';
+
+class EditTime extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    render() {
+        return (
+        <h1>Edit Time</h1>
+        )
+    }
+}
+
+export default EditTime;
