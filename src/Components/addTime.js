9274928d3026ce02b8d74ec3e0a359e9d69be1f7
Andrew Larson <andyson456@gmail.com>
2019-06-14 15:23:03 -0700

    Need to finish UI

--- src/Components/addTime.js
+++ src/Components/addTime.js
@@ -1,1 +1,18 @@
 import React, { Component } from 'react';
+
+class AddTime extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    render() {
+        return (
+        <h1>Add Time</h1>
+        )
+    }
+}
+
+export default AddTime;
