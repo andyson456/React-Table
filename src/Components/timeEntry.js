7bf634c7f091bff49ff4121f22d3c96fe6329407
Andrew Larson <andyson456@gmail.com>
2019-06-14 15:23:39 -0700

    Need to finish UI

--- src/Components/timeEntry.js
+++ src/Components/timeEntry.js
@@ -1,1 +1,18 @@
 import React, { Component } from 'react';
+
+class TimeEntry extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    render() {
+        return (
+        <h1>Time Entry</h1>
+        )
+    }
+}
+
+export default TimeEntry;
