8633eb9c9ff2d36788bf4f803e51596dd1613082
Andrew Larson <andyson456@gmail.com>
2019-06-19 14:43:07 -0700

    timeEntry component now routes editTime component

--- src/Components/timeEntry.js
+++ src/Components/timeEntry.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'reactstrap';
+import { NavLink, Route } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import routes from '../routes';
 
 class TimeEntry extends Component {
     constructor(props){
@@ -10,7 +15,13 @@ class TimeEntry extends Component {
 
     render() {
         return (
+        <NavItem>
         <h1>Time Entry</h1>
+            <NavLink className={'nav-link'} activeClassName={'active'}
+                to={routes.editTime}><Button variant="contained" color="primary">
+                <AddIcon />Edit Time</Button>
+            </NavLink>
+        </NavItem>
         )
     }
 }
