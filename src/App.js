302f25d2c48efd137cd6b9ffa795e6d6741282cd
Andrew Larson <andyson456@gmail.com>
2019-06-18 17:45:35 -0700

    Updated UI

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import TimeEntry from './Components/timeEntry';
 import EditTime from './Components/editTime';
 import AddTime from './Components/addTime';
 import Button from '@material-ui/core/Button';
-import AppBar from '@material-ui/core/AppBar';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import SettingsIcon from '@material-ui/icons/Settings';
+import FilterIcon from '@material-ui/icons/Filter';
 import AddIcon from '@material-ui/icons/Add';
+import Flexbox from 'flexbox-react';
 
 class App extends Component {
     constructor(){
@@ -23,15 +25,33 @@ class App extends Component {
         return(
         <div className="App">
             <h1><a className="navbar-brand" href="home">Employee Time Clock</a></h1>
-            <Button variant="contained" color="secondary">
-                <AccessAlarmIcon />
-                Clock Out
-            </Button>
+            <Flexbox display="flex" flexWrap="nowrap">
+                <Button variant="contained" color="secondary" m={2}>
+                    <AccessAlarmIcon />
+                    Clock Out
+                </Button>
             
-            <Button variant="contained" color="primary">
-                <AddIcon />
-                Add Time
-            </Button>
+                <Button variant="contained" color="primary">
+                    <AddIcon />
+                    Add Time
+                </Button>
+
+                <Flexbox display="flex" flexDirection="row-reverse">
+                    <Button variant="contained">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22 12l-4-4v3H3v2h15v3z"/></svg>								
+                        Push to Quickbooks</Button>
+                    <Button variant="contained">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 16.5l4-4h-3v-9h-2v9H8l4 4zm9-13h-6v1.99h6v14.03H3V5.49h6V3.5H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2v-14c0-1.1-.9-2-2-2z"/></svg>								
+                        Export</Button>
+                    <Button variant="contained">
+                        <SettingsIcon />
+                        Settings</Button>
+                    <Button variant="contained">
+                        <FilterIcon />
+                        Filter</Button>
+                </Flexbox>
+            </Flexbox>
+            
             <Navbar color="faded">
                 <Nav className="ml-auto" navbar>
                     <NavItem>
