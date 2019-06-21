656f2913b972c2dd302e695eca64f41c3ffec499
Andrew Larson <andyson456@gmail.com>
2019-06-21 15:38:12 -0700

    Added google maps

--- src/Components/timeEntry.js
+++ src/Components/timeEntry.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'reactstrap';
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import routes from '../routes';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+    width: '50%',
+    height: '50%'
+  };
+
 
 class TimeEntry extends Component {
     constructor(props){
@@ -15,15 +21,29 @@ class TimeEntry extends Component {
 
     render() {
         return (
-        <NavItem>
-        <h1>Time Entry</h1>
+        <div>
+            
+            <h1>Time Entry</h1>
             <NavLink className={'nav-link'} activeClassName={'active'}
                 to={routes.editTime}><Button variant="contained" color="primary">
                 <AddIcon />Edit Time</Button>
             </NavLink>
-        </NavItem>
+            
+            <Map
+            google={this.props.google}
+            zoom={14}
+            style={mapStyles}
+            initialCenter={{
+                lat: 44.0521,
+                lng: -123.0868
+            }}
+            />
+            </div>
         )
     }
 }
 
-export default TimeEntry;
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+export default GoogleApiWrapper({
+    apiKey: API_KEY })(TimeEntry);
